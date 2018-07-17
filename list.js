5a28c09237bc66699c22600de395d99731640587
Dani Roxberry <dani@bitoriented.com>
2018-07-17 10:14:42 -0700

    [wip] list endpoint isn't returning a list for me, figure out how to get all noteId

---
+++ list.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  console.log("[EVENT]", event);
+  const params = {
+    TableName: "note",
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId' partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id of the authenticated user
+    KeyConditionExpression: "userId = :userId AND noteId = :noteId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId,
+      ":noteId": "*"
+    }
+  };
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    // Return the matching list of items in response body
+    callback(null, success(result.Items));
+  } catch (e) {
+    console.log("[ERROR]", e);
+    callback(null, failure({ status: false }));
+  }
+}
