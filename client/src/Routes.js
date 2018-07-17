5c513a1bb98bf7923557bccfcc8a1935623897e4
Dani Roxberry <dani@bitoriented.com>
2018-07-17 16:37:26 -0700

    [add] complete fe with secure pages

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
@@ -11,15 +13,25 @@ import Notes from "./containers/Notes";
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute
+    <UnauthenticatedRoute
+      path="/login"
+      exact
+      component={Login}
+      props={childProps}
+    />
+    <UnauthenticatedRoute
+      path="/signup"
+      exact
+      component={Signup}
+      props={childProps}
+    />
+    <AuthenticatedRoute
       path="/notes/new"
       exact
       component={NewNote}
       props={childProps}
     />
-    <AppliedRoute
+    <AuthenticatedRoute
       path="/notes/:id"
       exact
       component={Notes}
