638b17f85a1470d5f85895fc0c1f702a97db74cb
Dani Roxberry <dani@bitoriented.com>
2018-07-17 09:48:13 -0700

    [fix] omg it works

--- create.js
+++ create.js
@@ -1,19 +1,24 @@
+import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 let data;
 
 export const main = async event => {
+  const eventData = JSON.parse(event.body);
   const params = {
     TableName: "note",
-    KeyConditionExpression: "userId = :userId",
-    ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId
+    Item: {
+      noteId: uuid.v1(),
+      userId: event.requestContext.identity.cognitoIdentityId,
+      content: eventData.content,
+      attachment: eventData.attachment,
+      createdAt: Date.now()
     }
   };
 
   try {
-    data = await dynamoDbLib.call("query", params);
+    data = await dynamoDbLib.call("put", params);
   } catch (err) {
     console.log(err);
     return failure({ status: false });
