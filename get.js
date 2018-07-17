1094c99393b7b098a831d7a6e39613aaadd1f6ff
Dani Roxberry <dani@bitoriented.com>
2018-07-17 09:59:00 -0700

    [add] GET /notes/{id} endpoint now works

---
+++ get.js
@@ -0,0 +1,33 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: "note",
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      // - 'userId': Identity Pool identity id of the authenticated user
+      userId: event.requestContext.identity.cognitoIdentityId,
+      // - 'noteId': path parameter
+      noteId: event.pathParameters.id
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved item
+      callback(null, success(result.Item));
+    } else {
+      callback(
+        null,
+        failure({
+          status: false,
+          error: "Item not found."
+        })
+      );
+    }
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
