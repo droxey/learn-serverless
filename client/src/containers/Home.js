0e3e50d3f46fe1b04e4fe2566688754d79f1c293
Dani Roxberry <dani@bitoriented.com>
2018-07-17 21:35:02 -0700

    [cleanup] rm old stuff

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { API } from "aws-amplify";
 import "./Home.css";
@@ -65,6 +66,14 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
